161c29ad92998a970ddbed876451854bc19663db
Isaac Chin <isaacchin@ucsb.edu>
2022-02-16 13:45:18 -0800

    Added entries for two placeholder pages on Storybook

---
+++ frontend/src/stories/pages/CollegiateSubreddits/CollegiateSubredditsIndexPage.stories.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+import CollegiateSubredditsIndexPage from "main/pages/CollegiateSubreddits/CollegiateSubredditsIndexPage";
+
+export default {
+    title: 'pages/CollegiateSubreddits/CollegiateSubredditsIndexPage',
+    component: CollegiateSubredditsIndexPage
+};
+
+const Template = () => <CollegiateSubredditsIndexPage />;
+
+export const Default = Template.bind({});
+
+
+
+

eeb03b19e2738dd90212b6d7aefe343faf96193d
Michael Glushchenko <glushchenko@cs.ucsb.edu>
2022-02-18 10:21:36 -0800

    mg - added storybook entries for UCSBSubjects Create, Edit, and Index Pages.

---
+++ frontend/src/stories/pages/UCSBSubjects/UCSBSubjectsCreatePage.stories.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+import UCSBDatesCreatePage from "main/pages/UCSBSubjects/UCSBSubjectsCreatePage";
+import UCSBSubjectsCreatePage from 'main/pages/UCSBSubjects/UCSBSubjectsCreatePage';
+
+export default {
+    title: 'pages/UCSBSubjects/UCSBSubjectsCreatePage',
+    component: UCSBSubjectsCreatePage
+};
+
+const Template = () => <UCSBSubjectsCreatePage />;
+
+export const Default = Template.bind({});
+
+
+
+
---
+++ frontend/src/stories/pages/UCSBSubjects/UCSBSubjectsEditPage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import UCSBSubjectsEditPage from 'main/pages/UCSBSubjects/UCSBSubjectsEditPage';
+
+export default {
+    title: 'pages/UCSBSubjects/UCSBSubjectsEditPage',
+    component: UCSBSubjectsEditPage
+};
+
+const Template = () => <UCSBSubjectsEditPage />;
+
+export const Default = Template.bind({});
+
+
+
+

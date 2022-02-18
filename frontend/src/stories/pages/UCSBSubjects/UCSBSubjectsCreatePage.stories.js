980f3214ff752c1fbfbabb428b003982f9daca7c
Michael Glushchenko <glushchenko@cs.ucsb.edu>
2022-02-18 10:26:17 -0800

    added storybook component

--- frontend/src/stories/pages/UCSBSubjects/UCSBSubjectsCreatePage.stories.js
+++ frontend/src/stories/pages/UCSBSubjects/UCSBSubjectsCreatePage.stories.js
@@ -1,7 +1,6 @@
 
 import React from 'react';
 
-import UCSBDatesCreatePage from "main/pages/UCSBSubjects/UCSBSubjectsCreatePage";
 import UCSBSubjectsCreatePage from 'main/pages/UCSBSubjects/UCSBSubjectsCreatePage';
 
 export default {

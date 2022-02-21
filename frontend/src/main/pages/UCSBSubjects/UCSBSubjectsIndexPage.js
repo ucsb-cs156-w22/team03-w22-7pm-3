6db6801d0b5fc6c4d48d96babdfca2c4ecaef0c9
Michael Glushchenko <glushchenko@cs.ucsb.edu>
2022-02-20 17:42:06 -0800

    mg - implemented list page for UCSB Subjects

--- frontend/src/main/pages/UCSBSubjects/UCSBSubjectsIndexPage.js
+++ frontend/src/main/pages/UCSBSubjects/UCSBSubjectsIndexPage.js
@@ -1,13 +1,27 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useCurrentUser } from 'main/utils/currentUser'
+import UCSBSubjectsTable from 'main/components/UCSBSubjects/UCSBSubjectsTable';
 
 export default function UCSBSubjectsIndexPage() {
+
+  const currentUser = useCurrentUser();
+
+  const { data: students, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/ucsbsubjects/all"],
+      { method: "GET", url: "/api/ucsbsubjects/all" },
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>UCSBSubjects</h1>
-        <p>
-          Temp page for UCSBSubjectsIndexPage
-        </p>
+        <UCSBSubjectsTable ucsbsubjects={ucsbsubjects} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )

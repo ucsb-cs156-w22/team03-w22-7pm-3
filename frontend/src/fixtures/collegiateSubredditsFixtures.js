8c7858f50b810a976f1468901396934947439896
Isaac Chin <isaacchin@ucsb.edu>
2022-02-20 17:46:55 -0800

    Added CollegiateSubredditsTable component with related fixtures, tests, and storybook entry

---
+++ frontend/src/fixtures/collegiateSubredditsFixtures.js
@@ -0,0 +1,31 @@
+const collegiateSubredditsFixtures = {
+    oneSubreddit: {
+        "id": 1,
+        "name": "UC Santa Barbara",
+        "location": "https://old.reddit.com/r/UCSantaBarbara/",
+        "subreddit": "UCSantaBarbara",
+      },
+    threeSubreddits: [
+        {
+            "id": 1,
+            "name": "UC Santa Barbara",
+            "location": "https://old.reddit.com/r/UCSantaBarbara/",
+            "subreddit": "UCSantaBarbara",
+          },
+          {
+            "id": 2,
+            "name": "UCLA",
+            "location": "https://old.reddit.com/r/ucla/",
+            "subreddit": "ucla",
+          },
+          {
+            "id": 3,
+            "name": "UC San Diego",
+            "location": "https://old.reddit.com/r/UCSD/",
+            "subreddit": "UCSD",
+          }
+    ]
+};
+
+
+export { collegiateSubredditsFixtures };

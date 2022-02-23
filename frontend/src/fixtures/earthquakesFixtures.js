4590fba5e2eb642a86189ed5261eea816e554379
[Xiyue Wang] <[xiyuewang@ucsb.edu]>
2022-02-23 11:55:41 -0800

    Fixed some small issues

--- frontend/src/fixtures/earthquakesFixtures.js
+++ frontend/src/fixtures/earthquakesFixtures.js
@@ -1,4 +1,4 @@
-const earthquakeFixtures = {
+const earthquakesFixtures = {
 	oneEarthquake: [
 		{
 			"id": "abcd1234abcd1234abcd1234abcd1234",
@@ -7,7 +7,30 @@ const earthquakeFixtures = {
 			"place": "10km ESE of Ojai, CA",
 			"time": 1644571919000
 		}
-	]
+	],
+	threeEarthquakes: [
+        {
+            "id": 1,
+            "title": "M 2.2 - 10km ESE of Ojai, CA",
+			"mag": 2.16,
+			"place": "10km ESE of Ojai, CA",
+			"time": 1644571919000
+        },
+        {
+            "id": 2,
+            "title": "M 2.2 - 10km ESE of Ojai, CA",
+			"mag": 2.16,
+			"place": "10km ESE of Ojai, CA",
+			"time": 1644571919000
+        },
+        {
+            "id": 3,
+            "title": "M 2.2 - 10km ESE of Ojai, CA",
+			"mag": 2.16,
+			"place": "10km ESE of Ojai, CA",
+			"time": 1644571919000
+        }
+    ]
 }
 
-export {earthquakeFixtures};
+export {earthquakesFixtures};

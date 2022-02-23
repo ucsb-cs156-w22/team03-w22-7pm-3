1982efa1fb32ca36e4709cbeeb2c31fdbaa1b506
[Xiyue Wang] <[xiyuewang@ucsb.edu]>
2022-02-23 11:17:57 -0800

    Xiyue-EQFixtures-and-table

---
+++ frontend/src/fixtures/earthquakesFixtures.js
@@ -0,0 +1,13 @@
+const earthquakeFixtures = {
+	oneEarthquake: [
+		{
+			"id": "abcd1234abcd1234abcd1234abcd1234",
+			"title": "M 2.2 - 10km ESE of Ojai, CA",
+			"mag": 2.16,
+			"place": "10km ESE of Ojai, CA",
+			"time": 1644571919000
+		}
+	]
+}
+
+export {earthquakeFixtures};

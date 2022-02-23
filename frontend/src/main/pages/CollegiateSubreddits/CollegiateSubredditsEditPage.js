9b1be3e32357a47a33b13a279c2a084660ac9748
Thomas H <Thomas@169-231-89-8.wireless.ucsb.edu>
2022-02-23 13:03:17 -0800

    th - did the edit page, now gotta do the tests for edit-page

---
+++ frontend/src/main/pages/CollegiateSubreddits/CollegiateSubredditsEditPage.js
@@ -0,0 +1,70 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
+import CollegiateSubredditsForm from "main/components/CollegiateSubreddits/CollegiateSubredditsForm";
+import { Navigate } from 'react-router-dom'
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
+
+export default function CollegiateSubredditsEditPage() {
+  let { id } = useParams();
+
+  const { data: subreddit, error: error, status: status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/collegiatesubreddits?id=${id}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/collegiatesubreddits`,
+        params: {
+          id
+        }
+      }
+    );
+
+
+  const objectToAxiosPutParams = (subreddit) => ({
+    url: "/api/collegiatesubreddits",
+    method: "PUT",
+    params: {
+      id: subreddit.id,
+    },
+    data: {
+      name: subreddit.name,
+      location: subreddit.location,
+      subreddit: subreddit.subreddit
+    }
+  });
+
+  const onSuccess = (subreddit) => {
+    toast(`CollegiateSubreddit Updated - id: ${subreddit.id} name: ${subreddit.name}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/collegiatesubreddit?id=${id}`]
+  );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess) {
+    return <Navigate to="/collegiatesubreddit/list" />
+  }
+
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Edit CollegiateSubreddit</h1>
+        {subreddit &&
+          <CollegiateSubredditsForm initialCollegiateSubreddit={subreddit} submitAction={onSubmit} buttonLabel="Update" />
+        }
+      </div>
+    </BasicLayout>
+  )
+}
+

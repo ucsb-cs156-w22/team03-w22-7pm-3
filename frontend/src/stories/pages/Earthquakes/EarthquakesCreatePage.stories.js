8afd93a65261b761fe477e5cc339e426868d3653
vChase42 <chasten@ucsb.edu>
2022-02-28 21:19:35 -0800

    BRING UP TO DATE

---
+++ frontend/src/stories/pages/Earthquakes/EarthquakesCreatePage.stories.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+import EarthquakesCreatePage from "main/pages/Earthquakes/EarthquakesCreatePage";
+
+export default {
+    title: 'pages/Earthquakes/EarthquakesCreatePage',
+    component: UCSBDatesCreatePage
+};
+
+const Template = () => <EarthquakesCreatePage />;
+
+export const Default = Template.bind({});
--- frontend/src/tests/components/Nav/AppNavbar.test.js
+++ frontend/src/tests/components/Nav/AppNavbar.test.js
@@ -213,7 +213,6 @@ describe("AppNavbar tests", () => {
 
     });
 
-    //
     test("renders the ucsbsubjects menu correctly for a user", async () => {
 
         const currentUser = currentUserFixtures.userOnly;
@@ -261,7 +260,54 @@ describe("AppNavbar tests", () => {
         await waitFor( () => expect(getByTestId(/appnavbar-ucsbsubjects-create/)).toBeInTheDocument() );
 
     });
-    //
+
+    test("renders the earthquakes menu correctly for a user", async () => {
+
+        const currentUser = currentUserFixtures.userOnly;
+        const systemInfo = systemInfoFixtures.showingBoth;
+
+        const doLogin = jest.fn();
+
+        const {getByTestId  } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AppNavbar currentUser={currentUser} systemInfo={systemInfo} doLogin={doLogin} />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => expect(getByTestId("appnavbar-earthquakes-dropdown")).toBeInTheDocument());
+        const dropdown = getByTestId("appnavbar-earthquakes-dropdown");
+        const aElement = dropdown.querySelector("a");
+        expect(aElement).toBeInTheDocument();
+        aElement?.click();
+        await waitFor( () => expect(getByTestId("appnavbar-earthquakes-list")).toBeInTheDocument() );
+
+    });
+
+    test("renders the earthquakes menu correctly for an admin", async () => {
+
+        const currentUser = currentUserFixtures.adminUser;
+        const systemInfo = systemInfoFixtures.showingBoth;
+
+        const doLogin = jest.fn();
+
+        const {getByTestId  } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AppNavbar currentUser={currentUser} systemInfo={systemInfo} doLogin={doLogin} />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => expect(getByTestId("appnavbar-earthquakes-dropdown")).toBeInTheDocument());
+        const dropdown = getByTestId("appnavbar-earthquakes-dropdown");
+        const aElement = dropdown.querySelector("a");
+        expect(aElement).toBeInTheDocument();
+        aElement?.click();
+        await waitFor( () => expect(getByTestId("appnavbar-earthquakes-retrieve")).toBeInTheDocument() );
+
+    });
 
     test("renders the Students menu correctly for a user", async () => {
 
